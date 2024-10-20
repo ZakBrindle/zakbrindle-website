2f9b10c18893aa0509bbbb56ab22555798e33134
Zak <z4kbrindle@gmail.com>
2024-10-20 22:17:04 +0100

    Delete kensington/AI directory

--- kensington/AI/js/badHireCostMenuButton.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("badHireCostBtn").addEventListener("click", function(){
-    window.open("badHireCost.html", "_blank", "width=800,height=545");
-});
-
-
-document.getElementById("badHireCostBtn").addEventListener("mouseover", function(){
-    this.style.cursor = "pointer";
-});
